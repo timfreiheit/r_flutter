7566de0dcf98a4357225a70541053ceacccc504f
Kostadin Damyanv <kostadin.damyanov@primeholding.com>
2021-10-05 18:17:58 +0300

    Implement generating keys for intl_features

--- lib/src/generator/i18n/generator.dart
+++ lib/src/generator/i18n/generator.dart
@@ -6,10 +6,13 @@ import 'package:r_flutter/src/model/i18n.dart';
 import 'delegate_generator.dart';
 import 'keys_generator.dart';
 
-List<DartClass> generateI18nClasses(I18nLocales i18n) {
+List<DartClass> generateI18nClasses(
+  I18nLocales i18n,
+  Map<String, I18nLocales>? i18nFeatures,
+) {
   return [
     generateI18nClass(i18n),
-    generateI18nKeysClass(i18n),
+    ...generateI18nKeysClasses(i18n, i18nFeatures),
     ...generateLookupClasses(i18n),
     generateI18nDelegate(i18n)
   ];
--- lib/src/generator/i18n/keys_generator.dart
+++ lib/src/generator/i18n/keys_generator.dart
@@ -1,5 +1,6 @@
 import 'package:r_flutter/src/model/dart_class.dart';
 import 'package:r_flutter/src/model/i18n.dart';
+import 'package:recase/recase.dart';
 
 ///
 /// ```dart
@@ -9,14 +10,58 @@ import 'package:r_flutter/src/model/i18n.dart';
 /// }
 /// ```
 ///
-DartClass generateI18nKeysClass(I18nLocales locales) {
-  final classString = StringBuffer("class I18nKeys {\n");
+List<DartClass> generateI18nKeysClasses(
+  I18nLocales locales, [
+  Map<String, I18nLocales>? i18nFeatures,
+]) {
+  final featureClassNames = <String, String>{};
+  final featureClasses = <DartClass>[];
+
+  i18nFeatures?.forEach((featureName, locales) {
+    final featureClassName = 'I18n${ReCase(featureName).pascalCase}Keys';
+    featureClassNames[featureName] = featureClassName;
+
+    featureClasses
+        .add(_generateI18nKeysClass(locales, className: featureClassName));
+  });
+
+  return <DartClass>[
+    _generateI18nKeysClass(locales, featureClassNames: featureClassNames),
+    ...featureClasses
+  ];
+}
+
+DartClass _generateI18nKeysClass(
+  I18nLocales locales, {
+  String? className,
+  Map<String, String>? featureClassNames,
+}) {
+  final classString = StringBuffer("class ${className ?? 'I18nKeys'} {\n");
+
+  if (className != null) {
+    classString.writeln('  const $className();');
+  }
 
   final items = locales.defaultValues.strings;
   for (final item in items) {
     classString
-        .writeln("  static const String ${item.escapedKey} = \"${item.key}\";");
+        .writeln('  static const String ${item.escapedKey} = "${item.key}";');
   }
+
+  featureClassNames?.forEach((featureName, featureClassName) {
+    final propertyName = ReCase(featureName).camelCase;
+
+    if (items
+        .any((e) => e.escapedKey.toLowerCase() == propertyName.toLowerCase())) {
+      throw StateError(
+        'There is a string and a feature with the same name: $propertyName',
+      );
+    }
+
+    classString.writeln(
+      '  static const $featureClassName $propertyName = $featureClassName();',
+    );
+  });
 
   classString.writeln("}");
   return DartClass(code: classString.toString());
